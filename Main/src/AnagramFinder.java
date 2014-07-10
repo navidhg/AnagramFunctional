0ed13ad6cb05bf3362582af79b58f2f67d6e359e
Navid Hallajian <navidhg@gmail.com>
2014-07-10 22:30:28 +0100

    Initial commit

---
+++ Main/src/AnagramFinder.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Created by navidhg on 09/07/2014.
+ */
+public class AnagramFinder {
+
+    public static void main (String[] args) {
+
+        List<String> names = Arrays.asList("john", "david", "peter", "sarah", "anna");
+        Collections.sort(names, (a, b) -> b.compareTo(a));
+
+        for (String s : names) System.out.println(s);
+    }
+
+}
